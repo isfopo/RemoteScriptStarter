8593753ed335946452aaa5365b05c84a7854a997
Isaac Poole <isaacpoolemusic@gmail.com>
2022-02-07 10:20:25 -0500

    adds another docs link

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,5 @@ from RemoteScriptStarter import RemoteScriptStarter
 
 
 def create_instance(c_instance):
+    ''' Creates and returns Remote Script instance '''
     return RemoteScriptStarter(c_instance)
