8e01e24363838548c6d51fa630b43a15258d61d9
Isaac Poole <isaacpoolemusic@gmail.com>
2022-02-08 09:55:21 -0500

    fixes suppressing rebuild request error

--- src/RemoteScriptStarter.py
+++ src/RemoteScriptStarter.py
@@ -6,15 +6,16 @@ from _Framework.ControlSurface import ControlSurface
 
 
 class RemoteScriptStarter(ControlSurface):
+
     def __init__(self, c_instance):
-        with self.suppressing_rebuild_requests():
-            ControlSurface.__init__(self, c_instance)
+        ControlSurface.__init__(self, c_instance)
+        with self.component_guard():
             live = Live.Application.get_application()
             self._live_major_version = live.get_major_version()
             self._live_minor_version = live.get_minor_version()
             self._live_bugfix_version = live.get_bugfix_version()
 
-            # write your init code here
+        # write your init code here
 
     def disconnect(self):
         """clean things up on disconnect"""
