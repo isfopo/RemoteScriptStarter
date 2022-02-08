87d68b72ded66a1d39cc6da5dfd78c5ba13d7cf6
Isaac Poole <isaacpoolemusic@gmail.com>
2022-02-08 08:56:52 -0500

    cleanup

--- watcher.py
+++ watcher.py
@@ -11,13 +11,11 @@ class Watcher(object):
     running = True
     refresh_delay_secs = 1
 
-    # Constructor
     def __init__(self, watch_file, callback=None):
         self._cached_stamp = 0
         self.filename = watch_file
         self.callback = callback
 
-    # Look for changes
     def look(self):
         stamp = os.stat(self.filename).st_mtime
         if stamp != self._cached_stamp:
@@ -25,22 +23,18 @@ class Watcher(object):
             if self.callback is not None:
                 self.callback(self.filename)
 
-    # Keep watching in a loop
     def watch(self):
         while self.running:
             try:
-                # Look for changes
                 time.sleep(self.refresh_delay_secs)
                 self.look()
             except KeyboardInterrupt:
                 print('\nStopped')
                 break
             except FileNotFoundError:
-                # Action on file not found
                 raise Exception("""
                 File not found, check your version number
                 and use 'Live {version} in the --version argument
                 """)
                 pass
 
-# Call this function each time a change happens
