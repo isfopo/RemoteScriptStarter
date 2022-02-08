01628e1e10ee6fe86d0938a1306a49d450ddc814
Isaac Poole <isaacpoolemusic@gmail.com>
2022-02-08 11:26:06 -0500

    initializes midi mappings

--- src/RemoteScriptStarter.py
+++ src/RemoteScriptStarter.py
@@ -3,6 +3,10 @@ from __future__ import with_statement
 import Live
 
 from _Framework.ControlSurface import ControlSurface
+from _Framework.SliderElement import SliderElement
+from _Framework.ButtonElement import ButtonElement
+
+from mappings import types, BUTTONCHANNEL, SLIDERCHANNEL
 
 
 class RemoteScriptStarter(ControlSurface):
@@ -15,9 +19,34 @@ class RemoteScriptStarter(ControlSurface):
             self._live_minor_version = live.get_minor_version()
             self._live_bugfix_version = live.get_bugfix_version()
 
-        # write your init code here
+            self._note_map = []
+            self._ctrl_map = []
+            self._load_MIDI_map()
+
+            # write your init code here
 
     def disconnect(self):
         """clean up on disconnect"""
         ControlSurface.disconnect(self)
         return None
+
+    def _load_mappings(self):
+        momentary = True
+
+        for note in range(128):
+            button = ButtonElement(momentary, types.NOTE, BUTTONCHANNEL, note)
+            button.name = 'Note_' + str(note)
+            self._note_map.append(button)
+        self._note_map.append(None)
+
+        for ctrl in range(128):
+            control = ButtonElement(momentary, types.CC, BUTTONCHANNEL, ctrl)
+            control.name = 'Ctrl_' + str(control)
+            self._ctrl_map.append(control)
+        self._note_map.append(None)
+
+        for ctrl in range(128):
+            control = SliderElement(types.CC, SLIDERCHANNEL, ctrl)
+            control.name = 'Ctrl_' + str(ctrl)
+            self._ctrl_map.append(control)
+        self._ctrl_map.append(None)
--- src/mappings.py
+++ src/mappings.py
@@ -3,8 +3,9 @@
   in order to be able to easily modify and change and MIDI notes or
   CCs that are used throughout your script.
 
-  On the main branch this file will be empty (except for this explanation)
-  but other branches should contain mappings.
+  On the main branch this file will only contain basic, assumed mappings
+  but other branches could contain more or less depending on what the
+  script need. Feel free to change these on your scripts as well.
 
   Example:
 
@@ -21,4 +22,20 @@
   from mappings import *
 
   with other import
+
+  These mappings are then "loaded" into script by initialing an array of
+  buttons and slider in the method `_load_mappings` which runs at __init__
+  and will load this array into `self._note_map` and `self._ctrl_map` for
+  Midi Notes and Midi CCs respectively.
+
+  Any assignment to `-1` is ignored.
 """
+
+
+class types:
+    NOTE = 0
+    CC = 1
+
+
+BUTTONCHANNEL = 0
+SLIDERCHANNEL = 1
