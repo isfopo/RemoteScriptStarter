d036c5197f77882aecd701f4b6a719b29162a3b1
Isaac Poole <isaacpoolemusic@gmail.com>
2022-02-08 10:51:04 -0500

    adds file for midi map constants

---
+++ src/mappings.py
@@ -0,0 +1,18 @@
+"""
+  This file can be used to create constants for your MIDI mappings
+  in order to be able to easily modify and change and MIDI notes or
+  CCs that are used throughout your script.
+
+  On the main branch this file will be empty (except for this explanation)
+  but other branches should contain mappings.
+
+  Example:
+
+  PLAY = 85
+  STOP = 60
+  REC = 86
+  TAPTEMPO = 3
+
+  As you can see, this is simply assigning number, that represent MIDI note
+  or CCs, depending on how they are used in the context of the script.
+"""
