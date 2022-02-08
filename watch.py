c6862457659fb7494a2f193512511b04da42e1e7
Isaac Poole <isaacpoolemusic@gmail.com>
2022-02-08 09:03:38 -0500

    moves Watcher class to watch.py

--- watch.py
+++ watch.py
@@ -15,11 +15,42 @@
 
 import os
 import re
+import time
 import getpass
 import argparse
 import platform
 
-from watcher import Watcher
+
+class Watcher(object):
+    running = True
+    refresh_delay_secs = 1
+
+    def __init__(self, watch_file, callback=None):
+        self._cached_stamp = 0
+        self.filename = watch_file
+        self.callback = callback
+
+    def look(self):
+        stamp = os.stat(self.filename).st_mtime
+        if stamp != self._cached_stamp:
+            self._cached_stamp = stamp
+            if self.callback is not None:
+                self.callback(self.filename)
+
+    def watch(self):
+        while self.running:
+            try:
+                time.sleep(self.refresh_delay_secs)
+                self.look()
+            except KeyboardInterrupt:
+                print('\nStopped')
+                break
+            except FileNotFoundError:
+                raise Exception("""
+                File not found, check your version number
+                and use 'Live {version} in the --version argument
+                """)
+
 
 ABLETONPATHWIN = "C:\\Users\\{user}\\AppData\\Roaming\\Ableton\\"
 
